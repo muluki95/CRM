115bf9861f27e767dc82c97cbca3e62b30cd281d
muluki95 <mulukiesther@gmail.com>
2024-01-13 16:55:16 -0600

    save

--- CRM/webapp/views.py
+++ CRM/webapp/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import CreateUserForm, LoginForm
 # Create your views here.
 
 def index(request):
     return render(request, 'webapp/index.html')
+
+#register a user
+def register(request):
+    form = CreateUserForm()
+    if request.method == "POST": #if the user has submitted the form
+        form = CreateUserForm(request.POST) #create a form with the data passed in
+        if form.is_valid(): #check to make sure all fields are filled out correctly
+            form.save() 
+           
+        return redirect('')
+    
+    context = {
+        'form': form,
+        }
+    return render(request, 'webapp/register.html', context)
+
 
