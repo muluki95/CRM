84801e8fad1e6cb0b28566c3cee9e2d3f74ac95a
muluki95 <mulukiesther@gmail.com>
2024-01-14 17:05:24 -0600

    save

--- CRM/webapp/urls.py
+++ CRM/webapp/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index ,name="index"),
+    path('', views.index , name="index"),
     path ('register/', views.register , name="register"),
+    path('login/', views.login, name="login"),
+    path('dashboard/', views.dashboard, name="dashboard"),
+     path('logout/', views.logout, name="logout"),
+    
+
 ]
--- CRM/webapp/views.py
+++ CRM/webapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import CreateUserForm, LoginForm
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def index(request):
@@ -14,11 +17,51 @@ def register(request):
         if form.is_valid(): #check to make sure all fields are filled out correctly
             form.save() 
            
-        return redirect('')
+        return redirect('login')
     
     context = {
         'form': form,
         }
     return render(request, 'webapp/register.html', context)
+
+# Login a user
+def login(request):
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid(): 
+
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request,username=username, password=password)
+
+            if user is not None:
+                auth.login(request,user)
+                return redirect('dashboard')
+    context = {
+                'form' : form,
+                }
+            
+    return render(request,'webapp/login.html',context)
+
+#dashboard
+@login_required(login_url='login') #decorator
+def dashboard(request):
+    return render(request,'webapp/dashboard.html')
+
+
+
+#logout
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+
+
+   
+
+
+            
 
 
